f8c97f136c3b1a4185f744dc516b50fdf9eb762e
David Schlegel <daveschlegel@gmail.com>
2021-07-15 06:38:13 -0700

    add directory reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
 import MenuItem from '../menu-item/menu-item.component';
 import './directoy.styles.scss';
 
-import SECTIONS_DATA from './sections.data';
+//import SECTIONS_DATA from './sections.data';
 
-class Directory extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-          sections: SECTIONS_DATA
-        }     
-    }
-
-            render() {
-                return (
-                    <div className='directory-menu'>
-                        {
-                          //otherSectionProps is equiv to adding all the props title, imageUrl, size
-                            this.state.sections.map(({id, ...otherSectionProps}) => (
-                                <MenuItem key={id} {...otherSectionProps} />
-                            ))
-                        }
-                    </div>
-                )  
-            }
+const Directory = ({ sections }) => (
+    <div className='directory-menu'>
+        {
+            //otherSectionProps is equiv to adding all the props title, imageUrl, size
+                sections.map(({id, ...otherSectionProps}) => (
+                <MenuItem key={id} {...otherSectionProps} />
+            ))
         }
+    </div>
+);
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
