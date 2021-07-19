323afc2df1e557ecb67d36717963bb08bd2d7e35
David Schlegel <daveschlegel@gmail.com>
2021-07-19 11:27:28 -0700

    moved directory and shop data into reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
+
 import './directoy.styles.scss';
 
-//import SECTIONS_DATA from './sections.data';
-
 const Directory = ({ sections }) => (
-    <div className='directory-menu'>
-        {
-            //otherSectionProps is equiv to adding all the props title, imageUrl, size
-                sections.map(({id, ...otherSectionProps}) => (
-                <MenuItem key={id} {...otherSectionProps} />
-            ))
-        }
-    </div>
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    sections: selectDirectorySections
+  sections: selectDirectorySections
 });
 
 export default connect(mapStateToProps)(Directory);
