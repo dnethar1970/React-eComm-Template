9d65fe74774003cd0ac49f1323e2d8b4aecad90b
David Schlegel <47117310+dnethar1970@users.noreply.github.com>
2021-08-09 10:52:41 -0700

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,14 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-        apiKey: 'AIzaSyA0UZCms6tB8kAbwRQgTY77kkgGAx9ucDY',
-        authDomain: 'react-ecomm-db-979a9.firebaseapp.com',
-        projectId: 'react-ecomm-db-979a9',
-        storageBucket: 'react-ecomm-db-979a9.appspot.com',
-        messagingSenderId: '920652423589',
-        appId: '1:920652423589:web:f8223bef5597d3d238884c',
-        measurementId: 'G-6Z2PTP5PYV'
+const config = {Your credentials here (Firebase configs)
 };
 
 export const createUserProfileDocument = async (userAuth, ...additionalData) => {
